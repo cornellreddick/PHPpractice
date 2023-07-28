f62480e227b37c0be8093117d59093e6c0bdf1ab
cornellreddick <cornellreddick@outlook.com>
2023-07-28 15:45:39 -0400

    Setup the update and delete for the form
    Signed-off-by: cornellreddick <cornellreddick@outlook.com>

--- select.php
+++ select.php
@@ -12,10 +12,15 @@ $result = $db->query($sql);
 
 foreach($result as $row){
     printf(
-        '<li><span style="color: %s">%s (%s)</span></li>',
+        '<li><span style="color: %s">%s (%s)</span>
+        <a href="update.php?id=%s">update</a>
+        <a href="delete.php?id=%s">delete</a>
+        </li>',
         htmlspecialchars($row['color'], ENT_QUOTES),
         htmlspecialchars($row['name'], ENT_QUOTES), 
-        htmlspecialchars($row['gender'], ENT_QUOTES));
+        htmlspecialchars($row['gender'], ENT_QUOTES),
+        htmlspecialchars($row['id'], ENT_QUOTES),
+        htmlspecialchars($row['id'], ENT_QUOTES));
 }
 
 $db->close();
